d610ccf82fdf255f98ae48fff4c1efb545ba2e13
dobinrutis <dobin@broken.ch>
2021-07-20 21:08:31 +0200

    feature: address on commandline for client, server

--- main.go
+++ main.go
@@ -10,18 +10,25 @@ import (
 
 func main() {
 	flagServer := flag.Bool("server", false, "IsServer")
+	flagServerAddr := flag.String("serveraddr", "127.0.0.1:4444", "Server listen address")
+
 	flagClient := flag.Bool("client", false, "IsClient")
-	flagAddr := flag.String("listenaddr", "127.0.0.1:4444", "Server listen address")
+	flagClientAddr := flag.String("clientaddr", "", "Server URL for the client")
 	flag.Parse()
 
 	fmt.Println("Antnium 0.1")
 
 	if *flagServer {
-		s := server.NewServer(*flagAddr)
+		s := server.NewServer(*flagServerAddr)
 		s.Serve()
 	}
 	if *flagClient {
 		c := client.NewClient()
+
+		// Overwrite our server url
+		if *flagClientAddr != "" {
+			c.Campaign.ServerUrl = *flagClientAddr
+		}
 		c.Start()
 	}
 }
