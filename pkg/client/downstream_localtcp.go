410e83401e49e5af2c3ca5f2d29f5e06e6995344
dobinrutis <dobin@broken.ch>
2021-07-31 21:19:58 +0200

    fix: crash when executor exited

--- pkg/client/downstream_localtcp.go
+++ pkg/client/downstream_localtcp.go
@@ -27,26 +27,32 @@ func (d *DownstreamLocaltcp) do(packet model.Packet) (model.Packet, error) {
 	packetEncoded, err := executor.EncodePacket(packet)
 	if err != nil {
 		log.Error("Error: ", err.Error())
+		packet.Response["error"] = err.Error()
+		return packet, err
 	}
-	d.conn.Write(packetEncoded)
+	_, err = d.conn.Write(packetEncoded)
+	if err != nil {
+		log.Error("Could not write: " + err.Error())
+		packet.Response["error"] = err.Error()
+		return packet, err
+	}
 	d.conn.Write([]byte("\n"))
 
 	// Wait for answer
 	jsonStr, err := bufio.NewReader(d.conn).ReadString('\n')
 	if err != nil {
 		log.Error("Could not read: " + err.Error())
+		packet.Response["error"] = err.Error()
+		return packet, err
 	}
 	packet, err = executor.DecodePacket(jsonStr)
 	if err != nil {
 		log.Error("Error: ", err.Error())
-	}
-
-	// Always send response, as it is synchronized
-	if err != nil {
 		packet.Response["error"] = err.Error()
+		return packet, err
 	}
 
-	return packet, err
+	return packet, nil
 }
 
 func (d *DownstreamLocaltcp) startServer() {
