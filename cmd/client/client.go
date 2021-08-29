3a7416ef5850a8c5afa3bf547a71ef477dbb917a
dobinrutis <dobin@broken.ch>
2021-08-29 17:56:28 +0200

    feature: client command line args

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,22 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/dobin/antnium/pkg/client"
 )
 
 func main() {
-	flagClientAddr := flag.String("clientaddr", "", "Server URL for the client")
+	flagServerUrl := flag.String("addr", "", "")
+	flagProxyUrl := flag.String("proxy", "", "")
 	flag.Parse()
 
 	fmt.Println("Antnium 0.1")
 
 	c := client.NewClient()
 
-	// Overwrite our server url
-	if *flagClientAddr != "" {
-		c.Campaign.ServerUrl = *flagClientAddr
+	// env can be overwritten with args
+	if os.Getenv("PROXY") != "" {
+		c.Campaign.ProxyUrl = os.Getenv("PROXY")
 	}
+	if os.Getenv("ADDR") != "" {
+		c.Campaign.ProxyUrl = os.Getenv("ADDR")
+	}
+	if *flagServerUrl != "" {
+		c.Campaign.ServerUrl = *flagServerUrl
+	}
+	if *flagProxyUrl != "" {
+		c.Campaign.ProxyUrl = *flagProxyUrl
+	}
+
 	c.Start()
 	c.Loop()
 
