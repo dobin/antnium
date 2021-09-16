12abd249aeeaffa430760fa2a4e597c1e8093fe5
dobinrutis <dobin@broken.ch>
2021-09-16 23:03:51 +0200

    test: connector test update

--- pkg/server/connector_test.go
+++ pkg/server/connector_test.go
@@ -15,13 +15,49 @@ func TestConnectorHttp(t *testing.T) {
 	// Server in background, checking via client
 	s := NewServer("127.0.0.1:" + port)
 	defer s.Shutdown()
-
-	s.Campaign.ClientUseWebsocket = true
-
-	// Make a example packet the client should receive
+	s.Campaign.ClientUseWebsocket = false
 	packetA := makeSimpleTestPacket(computerId, "001")
 	s.Middleware.FrontendAddNewPacket(packetA)
-	// make server go
+	go s.Serve()
+
+	// make client
+	client := client.NewClient()
+	client.Campaign.ServerUrl = "http://127.0.0.1:" + port
+	client.Campaign.ClientUseWebsocket = false
+	client.Config.ComputerId = computerId
+	client.Start()
+	defer client.Shutdown()
+
+	// expect packet to be received upon connection (its already added)
+	packetB := <-client.UpstreamManager.ChannelIncoming
+	if packetB.PacketId != "001" || packetB.ComputerId != computerId {
+		t.Error("Err")
+	}
+
+	// Add a test packet via Frontend REST
+	packetC := makeSimpleTestPacket(computerId, "002")
+	s.Middleware.FrontendAddNewPacket(packetC)
+
+	// Expect it
+	packetD := <-client.UpstreamManager.ChannelIncoming
+	if packetD.PacketId != "002" || packetD.ComputerId != computerId {
+		t.Error("Err")
+	}
+
+}
+
+func TestConnectorWs(t *testing.T) {
+	//defer goleak.VerifyNone(t)
+
+	port := "55145"
+	computerId := "computerid-23"
+
+	// Server in background, checking via client
+	s := NewServer("127.0.0.1:" + port)
+	s.Campaign.ClientUseWebsocket = true
+	packetA := makeSimpleTestPacket(computerId, "001")
+	s.Middleware.FrontendAddNewPacket(packetA)
+	defer s.Shutdown()
 	go s.Serve()
 
 	// make client
